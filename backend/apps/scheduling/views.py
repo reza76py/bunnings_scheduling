129907ff654d2e8e5ca9c51fc75c5911494eeeb8
Reza Mollagholipour <reza76py@gmail.com>
2026-05-24 15:50:59 +1000

    Exempt API views from CSRF

--- backend/apps/scheduling/views.py
+++ backend/apps/scheduling/views.py
@@ -1,14 +1,18 @@
 from rest_framework import viewsets
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 from .models import CountingSession, Supplier
 from .serializers import CountingSessionSerializer, SupplierSerializer
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class SupplierViewSet(viewsets.ModelViewSet):
 	queryset = Supplier.objects.all().order_by('-created_at')
 	serializer_class = SupplierSerializer
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class CountingSessionViewSet(viewsets.ModelViewSet):
 	queryset = CountingSession.objects.select_related('supplier').all().order_by('-created_at')
 	serializer_class = CountingSessionSerializer
